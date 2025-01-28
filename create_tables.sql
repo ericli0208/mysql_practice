189b0b916face10da07bc3eeb1ff2fbdb4e2672a
Eric Li <ericli0208@gmail.com>
2025-01-28 15:59:49 -0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -26,15 +26,12 @@ CREATE TABLE mentorships (
 RENAME TABLE co_employees TO employees;
 
 -- making several alterations to employees
-/*
 ALTER TABLE employees
   DROP COLUMN age, 
   ADD COLUMN salary FLOAT NOT NULL AFTER contact_number,
   ADD COLUMN years_in_company INT NOT NULL AFTER salary;
-*/
 
 -- making more alterations to mentorships
-/*
 ALTER TABLE mentorships
   DROP FOREIGN KEY fk2;
 
@@ -42,9 +39,31 @@ ALTER TABLE mentorships
   ADD CONSTRAINT fk2 FOREIGN KEY(mentee_id) 
     REFERENCES employees(id) ON DELETE CASCADE ON UPDATE CASCADE, -- in order to modify fk constraints, we need to drop the fk first and then add it back in
   DROP INDEX mm_constraint;
-*/ 
+
 
 -- deleting tables
 /*
 DROP TABLE [IF EXISTS] mentorships;
 */ 
+
+-- inserting data into employees
+INSERT INTO employees (em_name, gender, contact_number, salary, years_in_company)
+VALUES ('James Lee', 'M', '516-514-6568', 3500, 11),
+  ('Peter Pasternak', 'M', '845-644-7919', 6010, 10),
+  ('Ciara Couto', 'F', '845-641-5236', 3900, 8),
+  ('Walker Welch', 'M', NULL, 2500, 4),
+  ('Li Xiao Ting', 'F', '646-218-7733', 5600, 4), 
+  ('Joyce Jones', 'F', '523-172-2191', 8000, 3),
+  ('Jason Cerrone', 'M', '725-441-7172', 7980, 2),
+  ('Prudence Phelps', 'F', '546-312-5112', 11000, 2),
+  ('Larry Zucker', 'M', '817-267-9799', 3500, 1),
+  ('Serena Parker', 'F', '621-211-7342', 12000, 1); 
+-- note that id and date_created did not need to be specified due to their default values
+
+-- inserting data into mentorships
+INSERT INTO mentorships -- no need to specify column names, as we are using all of them
+VALUES (1, 2, 'Ongoing', 'SQF Limited'),
+  (1, 3, 'Past', 'Wayne Fibre'),
+  (2, 3, 'Ongoing', 'SQF Limited'),
+  (3, 4, 'Ongoing', 'SQF Limited'),
+  (6, 5, 'Past', 'Flynn Tech');
